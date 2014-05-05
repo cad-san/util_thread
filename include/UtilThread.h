d8cc79a5d0a023a57dafaaeb14be83b43dcfe41d
cad-san <cadsan1986@gmail.com>
2014-05-05 23:33:40 +0900

    fix to start thread with UtilThread#start()

--- include/UtilThread.h
+++ include/UtilThread.h
@@ -1,11 +1,29 @@
 #ifndef D_UTIL_THREAD_H
 #define D_UTIL_THREAD_H
 
+#include <pthread.h>
+
 class UtilThread
 {
 private:
     bool ready_flag_;
     bool active_flag_;
+
+    pthread_t main_thread_;
+    pthread_mutex_t msg_guard_;
+
+    pthread_cond_t active_req_;
+
+    void requestStarting();
+    void waitStarting();
+    void notifyStarting();
+
+    /* スレッドのメイン関数 */
+    void main();
+
+    /* スレッド起動用静的関数(pthreadの制約上必要) */
+    static void* launcher(void *obj);
+
 public:
     UtilThread();
     ~UtilThread();
--- src/UtilThread.cpp
+++ src/UtilThread.cpp
@@ -4,6 +4,7 @@ UtilThread::UtilThread() :
     ready_flag_(false),
     active_flag_(false)
 {
+    pthread_mutex_init(&msg_guard_, NULL);
 }
 
 UtilThread::~UtilThread()
@@ -12,13 +13,16 @@ UtilThread::~UtilThread()
 
 bool UtilThread::init()
 {
+    pthread_cond_init(&active_req_, NULL);
+
     this->ready_flag_ = true;
     return true;
 }
 
 bool UtilThread::start()
 {
-    this->active_flag_ = true;
+    requestStarting();
+    waitStarting();
     return true;
 }
 
@@ -26,6 +30,34 @@ bool UtilThread::stop()
 {
     this->active_flag_ = false;
     return true;
+}
+
+void UtilThread::main()
+{
+    notifyStarting();
+    return;
+}
+
+void UtilThread::requestStarting()
+{
+    pthread_create(&this->main_thread_, NULL, UtilThread::launcher, this);
+}
+
+void UtilThread::waitStarting()
+{
+    pthread_mutex_lock(&msg_guard_);
+    while(!isActive())
+        pthread_cond_wait(&active_req_, &msg_guard_);
+    pthread_mutex_unlock(&msg_guard_);
+}
+
+void UtilThread::notifyStarting()
+{
+    pthread_mutex_lock(&msg_guard_);
+    this->active_flag_ = true;
+    pthread_mutex_unlock(&msg_guard_);
+
+    pthread_cond_signal(&this->active_req_);
 }
 
 bool UtilThread::isReady() const
@@ -36,4 +68,11 @@ bool UtilThread::isReady() const
 bool UtilThread::isActive() const
 {
     return this->active_flag_;
+}
+
+void* UtilThread::launcher(void* obj)
+{
+    UtilThread* thread = reinterpret_cast<UtilThread *>(obj);
+    thread->main();
+    return NULL;
 }
