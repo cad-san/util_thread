aadde26bc6c12ec4a4fb973a45f9249121d8f4c8
cad-san <cadsan1986@gmail.com>
2014-09-24 00:37:46 +0900

    fix with formatter

--- sample/server.cpp
+++ sample/server.cpp
@@ -9,60 +9,60 @@
 class SockRunner : public UtilRunner
 {
 private:
-	IpcSockServer sock_;
+    IpcSockServer sock_;
 
 public:
-	SockRunner(const std::string& path, int queue_size, const UtilTime& timeout) :
-		sock_(path, queue_size, timeout)
-	{
-	}
+    SockRunner(const std::string& path, int queue_size, const UtilTime& timeout) :
+        sock_(path, queue_size, timeout)
+    {
+    }
 
-	~SockRunner()
-	{
-		sock_.stop();
-	}
+    ~SockRunner()
+    {
+        sock_.stop();
+    }
 
-	void init()
-	{
-		sock_.init();
-		
-		sock_.start();
-	}
-	
-	void step()
-	{
-		char buffer[2048];
-		size_t recv_size = sock_.recv(buffer, 2048);
-		if(recv_size > 0 && recv_size < 2048)
-		{
-		    buffer[recv_size] = '\0';
-		    printf("receive message : %s\n",buffer);
-		}
-	}
+    void init()
+    {
+        sock_.init();
+
+        sock_.start();
+    }
+
+    void step()
+    {
+        char buffer[2048];
+        size_t recv_size = sock_.recv(buffer, 2048);
+        if(recv_size > 0 && recv_size < 2048)
+        {
+            buffer[recv_size] = '\0';
+            printf("receive message : %s\n",buffer);
+        }
+    }
 };
 
 typedef std::shared_ptr<SockRunner> SockRunnerPtr;
 
 int main()
 {
-	UtilTime timeout;
-	timeout.tv_sec = 0;
-	timeout.tv_nsec = 500 * UtilTime::MSEC_BASE;
+    UtilTime timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_nsec = 500 * UtilTime::MSEC_BASE;
 
-	SockRunnerPtr sock = std::make_shared<SockRunner>(SOCK_PATH, 128, timeout);
-	UtilThread sock_thread(sock);
+    SockRunnerPtr sock = std::make_shared<SockRunner>(SOCK_PATH, 128, timeout);
+    UtilThread sock_thread(sock);
 
-	sock_thread.setIntervalMiliSec(500);
+    sock_thread.setIntervalMiliSec(500);
 
-	printf("start server\n");
+    printf("start server\n");
 
-	sock_thread.init();
-	sock_thread.start();
+    sock_thread.init();
+    sock_thread.start();
 
-	printf("hit key to exit...\n");
-	getchar();
+    printf("hit key to exit...\n");
+    getchar();
 
-	sock_thread.stop();
+    sock_thread.stop();
 
-	return 0;
+    return 0;
 }
