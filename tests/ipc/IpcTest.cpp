db43adf3d557c521f7abf1978d31646db082374f
cad-san <cadsan1986@gmail.com>
2014-10-22 22:47:49 +0900

    add single test for IpcSockServer class

---
+++ tests/ipc/IpcTest.cpp
@@ -0,0 +1,33 @@
+#include "CppUTest/TestHarness.h"
+
+#include "IpcSockServer.h"
+#include "IpcSocKClient.h"
+#include <future>
+
+static const std::string dummy_path = "/tmp/test-socket";
+static const int dummy_dueue_size = 128;
+
+TEST_GROUP(Ipc)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(Ipc, ServerSingle)
+{
+    UtilTime timeout = 0.5;
+    IpcSockServer server(dummy_path, dummy_dueue_size, timeout);
+
+    bool initted = server.init();
+    bool started = server.start();
+    bool stoped = server.stop();
+
+    CHECK_EQUAL(true, initted);
+    CHECK_EQUAL(true, started);
+    CHECK_EQUAL(true, stoped);
+}
